d97ab64023e9e85eb224dcf1174688847614c192
Juan A. de la Puente <jpuente@dit.upm.es>
2016-12-16 12:03:08 +0100

    Añadidos comentarios

--- src/es/upm/dit/adsw/factorial/FactorialIterativo.java
+++ src/es/upm/dit/adsw/factorial/FactorialIterativo.java
@@ -2,11 +2,18 @@ package es.upm.dit.adsw.factorial;
 
 /**
  * Cálculo del factorial mediante un método iterativo
- *
+ * <p>
  * Created by jpuente on 14/12/16.
  */
 public class FactorialIterativo implements Factorial {
 
+    /**
+     * Calcular factorial de un número entero
+     *
+     * @param n entero positivo
+     * @return n!
+     * @throws IllegalArgumentException si n es negativo
+     */
     public long f(int n) {
         if (n < 0)
             throw new IllegalArgumentException();
--- src/es/upm/dit/adsw/factorial/FactorialRecursivo.java
+++ src/es/upm/dit/adsw/factorial/FactorialRecursivo.java
@@ -2,13 +2,20 @@ package es.upm.dit.adsw.factorial;
 
 /**
  * Cálculo del factorial mediante un método recursivo
- *
+ * <p>
  * Created by jpuente on 14/12/16.
  */
 public class FactorialRecursivo implements Factorial {
 
+    /**
+     * Calcular factorial de un número entero
+     *
+     * @param n entero positivo
+     * @return n!
+     * @throws IllegalArgumentException si n es negativo
+     */
     public long f(int n) {
-        if ( n > 0 ) {
+        if (n > 0) {
             return n * f(n - 1);
         } else if (n == 0) {
             return 1;
